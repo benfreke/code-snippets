c6a2e86e3db240a5ce257da73d3bd46ff81f1c3c
Ben Freke <benfreke@gmail.com>
2012-11-05 10:27:13 +1000

    Added attribution for LESS php stuff

--- PHP/less.php
+++ PHP/less.php
@@ -6,6 +6,8 @@
  * 
  * I suppose technically everything can go in the one file, but for neatness 
  *  I think it's better to separate the two
+ * 
+ * Far more information, and the full code, is available here: http://leafo.net/lessphp
  */
 
 
@@ -15,13 +17,13 @@
 if ($this->debug) { // A basic check so you don't have to change code between servers. Just change the config value.
     $filePath = $_SERVER['DOCUMENT_ROOT'];
     include( $filePath . '/includes/autoCompileLess.php');   
-    auto_compile_less( $filePath . '/css/less/style.less', $filePath . '/css/styleCompiled.css');
+    autoCompileLess( $filePath . '/css/less/style.less', $filePath . '/css/styleCompiled.css');
 }
 
 /**
  * This is the entirety of the autoCompileLess.php file
  */
-// Our helper class
+// Our helper class, http://leafo.net/lessphp
 require 'lessc.inc.php';
 
 /**
@@ -29,7 +31,7 @@ require 'lessc.inc.php';
  * @param string $less_fname The main less file which includes everything
  * @param string $css_fname The final compiled css filename.
  */
-function auto_compile_less($less_fname, $css_fname) {
+function autoCompileLess($less_fname, $css_fname) {
     $cache = $less_fname;
    
     // We may want to load from the cache
