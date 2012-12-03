03af21aefdfb5ca2a6023473ef240a06399a3b80
Ben Freke <benfreke@gmail.com>
2012-12-03 16:37:26 +1000

    Minor formatting fixes ... thanks PHP Storm!

--- PHP/less.php
+++ PHP/less.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Basic PHP to auto compile LESS on the server with no thinking required.
- * Once it is setup it works perfectly. If there are any issues (especially when moving between servers), 
+ * Once it is setup it works perfectly. If there are any issues (especially when moving between servers),
  *  delete the cached file and try again.
- * 
- * I suppose technically everything can go in the one file, but for neatness 
+ *
+ * I suppose technically everything can go in the one file, but for neatness
  *  I think it's better to separate the two
- * 
+ *
  * Far more information, and the full code, is available here: http://leafo.net/lessphp
  */
 
@@ -16,8 +16,8 @@
  */
 if ($this->debug) { // A basic check so you don't have to change code between servers. Just change the config value.
     $filePath = $_SERVER['DOCUMENT_ROOT'];
-    include( $filePath . '/includes/autoCompileLess.php');   
-    autoCompileLess( $filePath . '/css/less/style.less', $filePath . '/css/styleCompiled.css');
+    include($filePath . '/includes/autoCompileLess.php');
+    autoCompileLess($filePath . '/css/less/style.less', $filePath . '/css/styleCompiled.css');
 }
 
 /**
@@ -31,12 +31,13 @@ require 'lessc.inc.php';
  * @param string $less_fname The main less file which includes everything
  * @param string $css_fname The final compiled css filename.
  */
-function autoCompileLess($less_fname, $css_fname) {
+function autoCompileLess($less_fname, $css_fname)
+{
     $cache = $less_fname;
-   
+
     // We may want to load from the cache
-    $cache_fname = $less_fname.".cache";
-    if (file_exists($cache_fname)) {       
+    $cache_fname = $less_fname . ".cache";
+    if (file_exists($cache_fname)) {
         // Use the cache if the filetime is newer
         $cacheTime = filemtime($cache_fname);
         $fileTime = filemtime($less_fname);
@@ -47,7 +48,7 @@ function autoCompileLess($less_fname, $css_fname) {
     }
 
     $new_cache = lessc::cexecute($cache);
-    if (!is_array($cache) || $new_cache['updated'] > $cache['updated']) {   
+    if (!is_array($cache) || $new_cache['updated'] > $cache['updated']) {
         // Our final, compiled styles
         file_put_contents($css_fname, $new_cache['compiled']);
         // Cache it, so we load faster!
